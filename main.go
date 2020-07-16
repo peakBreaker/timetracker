4f2faa3b144101d97608a16587f64b88f7784331
Anders L. Hurum <andershurum@gmail.com>
2020-07-16 15:19:16 +0200

    enh: outlined app, added test

--- main.go
+++ main.go
@@ -4,6 +4,32 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type Tracker struct{}
+
+func (t Tracker) Track() {
+	log.Info("Starting timetracking")
+}
+
+func (t Tracker) End() {
+	log.Info("Ending timetracking")
+}
+
+func List() []string {
+	log.Info("Listing entries")
+
+	codes := []string{"code1", "code2"}
+	return codes
+}
+
+func Edit(hashCode string) {
+	log.Info("Editing " + hashCode)
+}
+
+func EditLast() {
+	log.Info("Starting timetracking")
+}
+
 func main() {
-	log.Warn("vim-go")
+	t := Tracker{}
+	t.Track()
 }
---
+++ main_test.go
@@ -0,0 +1,8 @@
+package main
+
+import "testing"
+
+func TestTrack(t *testing.T) {
+	tr := Tracker{}
+	tr.Track()
+}
